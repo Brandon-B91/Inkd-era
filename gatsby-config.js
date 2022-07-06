54afbe19ccb79dabe31b6452b2827628eed4a97c
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-07-06 16:19:33 -0600

    changes to packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,14 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-advanced-sitemap`,
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://www.inkdera.com',
-        sitemap: 'https://www.inkdera.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
+    // {
+    //   resolve: 'gatsby-plugin-robots-txt',
+    //   options: {
+    //     host: 'https://www.inkdera.com',
+    //     sitemap: 'https://www.inkdera.com/sitemap.xml',
+    //     policy: [{ userAgent: '*', allow: '/' }]
+    //   }
+    // },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
